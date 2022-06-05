feba49bcd6f7a09928f9631254bd98be01b946d2
makkii-bcr <makkii@mkbcr.com>
2022-06-05 23:55:49 +0900

    [conv]依存パッケージ更新、tsフォーマッタ、ビルドエラー修正

--- tool/base-conv.ts
+++ tool/base-conv.ts
@@ -1,65 +1,65 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import marked from 'marked';
-import zlib from 'zlib';
+import * as fs from "fs";
+import * as path from "path";
+import { marked } from "marked";
+import * as zlib from "zlib";
 
 /**
  * mdファイルを読み込んで、変換、HTMLデータを返す
- * @param htmlPath 
+ * @param htmlPath
  * @returns htmlのデータ
  */
 export function convMdToHtml(htmlPath: string): string {
-    const mdData = fs.readFileSync(htmlPath);
-    const htmlData = marked(mdData.toString());
-    return htmlData;
+  const mdData = fs.readFileSync(htmlPath);
+  const htmlData = marked(mdData.toString());
+  return htmlData;
 }
 
 /**
  * mdファイルパスをhtmlファイルパスにする
- * @param mdDir 
- * @param mdPath 
- * @returns 
+ * @param mdDir
+ * @param mdPath
+ * @returns
  */
 export function toHtmlPath(mdPath: string): string {
-    return path.parse(mdPath).name + '.html';
+  return path.parse(mdPath).name + ".html";
 }
 
 /**
  * ファイルの一覧を返す（サブフォルダ内のファイル含む）
- * @param dir 
+ * @param dir
  * @returns ファイルパス配列（絶対パス）
  */
 export function lsrSync(dir: string): string[] {
-    const retAry: string[] = [];
-    const pathList = fs.readdirSync(dir);
-    pathList.map(p => path.join(dir, p))
-        .forEach(p => {
-            if (fs.statSync(p).isDirectory()) {
-                lsrSync(p).forEach(pp => {
-                    retAry.push(pp);
-                });
-            } else {
-                retAry.push(p);
-            }
+  const retAry: string[] = [];
+  const pathList = fs.readdirSync(dir);
+  pathList.map((p) => path.join(dir, p))
+    .forEach((p) => {
+      if (fs.statSync(p).isDirectory()) {
+        lsrSync(p).forEach((pp) => {
+          retAry.push(pp);
         });
-    return retAry;
+      } else {
+        retAry.push(p);
+      }
+    });
+  return retAry;
 }
 
 /**
  * <head><title>に入れる文字列を取得する（<h1>～<h3>の文字列を取得）
- * @param htmlData 
- * @returns 
+ * @param htmlData
+ * @returns
  */
 export function getTitle(htmlData: string): string {
-    const start = htmlData.match(/<h[1-3]\s*.*?>/);
-    const end = htmlData.match(/<\/h[1-3]>/);
-    let title = '';
-    if (start != null && end != null) {
-        const startPos = start.index! + start[0].length;
-        const endPos = end.index;
-        title = htmlData.substring(startPos, endPos);
-    }
-    return title;
+  const start = htmlData.match(/<h[1-3]\s*.*?>/);
+  const end = htmlData.match(/<\/h[1-3]>/);
+  let title = "";
+  if (start != null && end != null) {
+    const startPos = start.index! + start[0].length;
+    const endPos = end.index;
+    title = htmlData.substring(startPos, endPos);
+  }
+  return title;
 }
 
 /**
@@ -67,18 +67,21 @@ export function getTitle(htmlData: string): string {
  * @param srcFilePath 比較元ファイルパス
  * @param destFilePath 比較先ファイルパス
  */
-export function isUpdateFile(srcFilePath: string, destFilePath: string): boolean {
-    if (!fs.existsSync(srcFilePath) || !fs.existsSync(destFilePath)) {
-        return true;
-    }
-    let srcTime = fs.statSync(srcFilePath).mtime.getTime();
-    let destTime = fs.statSync(destFilePath).mtime.getTime();
-    return srcTime > destTime;
+export function isUpdateFile(
+  srcFilePath: string,
+  destFilePath: string,
+): boolean {
+  if (!fs.existsSync(srcFilePath) || !fs.existsSync(destFilePath)) {
+    return true;
+  }
+  let srcTime = fs.statSync(srcFilePath).mtime.getTime();
+  let destTime = fs.statSync(destFilePath).mtime.getTime();
+  return srcTime > destTime;
 }
 
 export const CompressMode = {
-    Gzip: 'gzip',
-    Brotli: 'brotli',
+  Gzip: "gzip",
+  Brotli: "brotli",
 } as const;
 type CompressMode = typeof CompressMode[keyof typeof CompressMode];
 
@@ -90,41 +93,40 @@ type CompressMode = typeof CompressMode[keyof typeof CompressMode];
  * @returns {filePath, output}
  */
 export function compress(mode: CompressMode, filePath: string, data: string): {
-    filePath: string,
-    output: Buffer
+  filePath: string;
+  output: Buffer;
 } {
-    let output: Buffer;
-    if (mode == CompressMode.Gzip) {
-        output = gzipSync(data);
-        filePath += ".gz";
-    } else if (mode == CompressMode.Brotli) {
-        output = brotliSync(data);
-        filePath += ".br";
-    } else {
-        output = output!;
-    }
-    return { filePath, output };
+  let output: Buffer;
+  if (mode == CompressMode.Gzip) {
+    output = gzipSync(data);
+    filePath += ".gz";
+  } else if (mode == CompressMode.Brotli) {
+    output = brotliSync(data);
+    filePath += ".br";
+  } else {
+    output = output!;
+  }
+  return { filePath, output };
 }
 
 /**
  * gzip圧縮
- * @param data 
- * @returns 
+ * @param data
+ * @returns
  */
 export function gzipSync(data: string): Buffer {
-    const output = zlib.gzipSync(data, {
-        level: 9
-    });
-    return output;
+  const output = zlib.gzipSync(data, {
+    level: 9,
+  });
+  return output;
 }
 
 /**
  * brotli圧縮
- * @param data 
- * @returns 
+ * @param data
+ * @returns
  */
 export function brotliSync(data: string): Buffer {
-    const output = zlib.brotliCompressSync(data, {});
-    return output;
+  const output = zlib.brotliCompressSync(data, {});
+  return output;
 }
-
--- tool/conv.ts
+++ tool/conv.ts
@@ -1,233 +1,249 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import marked from 'marked';
-import imageSize from 'image-size';
-import { minify } from 'html-minifier-terser';
-import { performance } from 'perf_hooks';
-import * as base from './base-conv';
+import * as fs from "fs";
+import * as path from "path";
+import { marked } from "marked";
+import imageSize from "image-size";
+import { minify } from "html-minifier-terser";
+import { performance } from "perf_hooks";
+import * as base from "./base-conv";
 
-const repoName = 'Tonyu1-wiki';
-const rootPubDirName = 'docs';
-const testPubDirName = path.join('docs', repoName);
-const deployPubDirName = 'docs';
-const mdDirName = 'md';
-const templateDirName = 'template';
-const mdTemplateDirName = path.join('md', 'template');
+const repoName = "Tonyu1-wiki";
+const rootPubDirName = "docs";
+const testPubDirName = path.join("docs", repoName);
+const deployPubDirName = "docs";
+const mdDirName = "md";
+const templateDirName = "template";
+const mdTemplateDirName = path.join("md", "template");
 
 convMain();
 
 function convMain() {
-    console.log('run conv.ts');
+  console.log("run conv.ts");
 
-    const time3 = performance.now();
+  const time3 = performance.now();
 
-    const isDeploy = process.argv.some((v) => v === '--deploy');
-    const isStaging = process.argv.some((v) => v === '--staging');
-    const isHelpFile = process.argv.some((v) => v === '--helpfile');
-    const isFullBuild = isDeploy || isStaging || isHelpFile;
-    const curDir = process.cwd();
-    const pubDirName = isDeploy ? deployPubDirName : testPubDirName;
+  const isDeploy = process.argv.some((v) => v === "--deploy");
+  const isStaging = process.argv.some((v) => v === "--staging");
+  const isHelpFile = process.argv.some((v) => v === "--helpfile");
+  const isFullBuild = isDeploy || isStaging || isHelpFile;
+  const curDir = process.cwd();
+  const pubDirName = isDeploy ? deployPubDirName : testPubDirName;
 
-    // docsを全削除
-    if (isFullBuild) {
-        try {
-            // node v14
-            fs.rmSync(path.join(curDir, 'docs'), { recursive: true, force: true });
-        } catch (e) {
-            //fs.rmdirSync(path.join(curDir, 'docs'), { recursive: true });
-        }
+  // docsを全削除
+  if (isFullBuild) {
+    try {
+      // node v14
+      fs.rmSync(path.join(curDir, "docs"), { recursive: true, force: true });
+    } catch (e) {
+      //fs.rmdirSync(path.join(curDir, 'docs'), { recursive: true });
+    }
+  }
+
+  const mdDir = path.join(curDir, mdDirName);
+  const mdPaths = base.lsrSync(mdDir);
+
+  // テンプレートhtml, js, css
+  const tmplPath = path.join(curDir, templateDirName, "template.html");
+  const tmplHtmlData = Buffer.from(fs.readFileSync(tmplPath)).toString();
+  const tmplJsPath = path.join(curDir, templateDirName, "template.js");
+  const tmplJsData = Buffer.from(fs.readFileSync(tmplJsPath)).toString();
+  const tmplCssPath = path.join(curDir, templateDirName, "style.css");
+  const tmplCssData = Buffer.from(fs.readFileSync(tmplCssPath)).toString();
+
+  // 共通部品md
+  const sidebarPath = path.join(mdTemplateDirName, "tp-sidebar.md");
+  const sidebarHtmlData = base.convMdToHtml(sidebarPath);
+  const navPcPath = path.join(mdTemplateDirName, "tp-nav-pc.md");
+  const navPcHtmlData = base.convMdToHtml(navPcPath);
+  const navMobilePath = path.join(mdTemplateDirName, "tp-nav-mobile.md");
+  const navMobileHtmlData = base.convMdToHtml(navMobilePath);
+  const footerPath = path.join(mdTemplateDirName, "tp-footer.md");
+  const footerHtmlData = base.convMdToHtml(footerPath);
+
+  const htmlAry: { name: string; mdPath: string; data: string }[] = [];
+  const imgAry: { path: string; width: number; height: number }[] = [];
+
+  const dlDirPath = path.join(curDir, "md", "dl");
+
+  // mdディレクトリ読み込み
+  mdPaths.forEach((mdPath) => {
+    if (mdPath.match(/.md$/)) {
+      if (mdPath.match(/tp-.*.md$/)) {
+        return;
+      }
+      // markdown -> html 変換
+      const mdData = fs.readFileSync(mdPath);
+      let htmlData = marked(mdData.toString());
+
+      const mdName = path.parse(mdPath).name;
+      htmlAry.push({
+        name: mdName,
+        mdPath: mdPath,
+        data: htmlData,
+      });
+
+      console.log("convert md:", mdPath);
+    } else {
+      // dlフォルダはデプロイ時のみコピー
+      if (!isDeploy && mdPath.indexOf(dlDirPath) != -1) {
+        return;
+      }
+      // 画像のwidth,heightを取得する
+      if (path.parse(mdPath).ext == ".png") {
+        const imgSize = imageSize(mdPath);
+        imgAry.push({
+          path: mdPath,
+          width: imgSize.width || 0,
+          height: imgSize.height || 0,
+        });
+      }
+      const relativePath = mdPath.replace(mdDir, "");
+      const inPath = path.join(mdDir, relativePath);
+      const outPath = path.join(curDir, pubDirName, relativePath);
+      if (isFullBuild || base.isUpdateFile(inPath, outPath)) {
+        // ファイルコピー(md -> docs)
+        fs.mkdirSync(path.dirname(outPath), { recursive: true });
+        fs.copyFileSync(inPath, outPath);
+        console.log("copy file :", outPath);
+      }
+    }
+  });
+
+  const time2 = performance.now();
+
+  // htmlを生成
+  htmlAry.forEach(async (htmlObj) => {
+    // デバッグ時は404.htmlのみ作成
+    if (!(isDeploy || isStaging || isHelpFile || htmlObj.name == "404")) {
+      return;
+    }
+    const time = performance.now();
+
+    // 全htmlをまとめたhtmlを生成（content部分のみ）
+    const fileName = htmlObj.name;
+    let titleHtmlData = "";
+    let htmlDataSum = "";
+    let allPageMd = "";
+    htmlAry.forEach((obj) => {
+      const name = obj.name;
+      const isSelfPage = name == fileName;
+      // ヘルプファイルの場合は、1つのhtmlに1つのhtmlのみ入れる（1対1）
+      if (isHelpFile && !isSelfPage) {
+        return;
+      }
+      const displayStyle = isSelfPage ? "block" : "none";
+
+      let htmlData = obj.data;
+
+      // title取得
+      let title = base.getTitle(htmlData);
+      htmlData = htmlData.replace(
+        /\<title\>(.*)\<\/title\>/g,
+        (match, p1, offset, string) => {
+          title = p1;
+          return "";
+        },
+      );
+      // title末尾にサイト名を付ける
+      let titleAndSiteName = title;
+      if (name != "index") {
+        titleAndSiteName += " - Tonyu1 Wiki";
+      }
+      if (isSelfPage) { // 最初に表示するページ
+        titleHtmlData = titleAndSiteName;
+      }
+
+      // 非表示のページはimgタグsrcをsrc-tにして画像を読まないようにする
+      // 表示時にjs側でsrcに戻す
+      htmlData = htmlData.replace(
+        /src=\"(.*?)\"/g,
+        (match, p1, offset, string) => {
+          // <img src="xxx.png">を<img src-t="xxx.png">にする
+          // imgタグにwidth,heightを追加する
+          const img = imgAry.find((obj) =>
+            path.join(mdDir, p1).indexOf(obj.path) != -1
+          );
+          const width = img ? ' width="' + img.width + '" ' : "";
+          const height = img ? ' height="' + img.height + '" ' : "";
+          if (isSelfPage) {
+            return 'src="' + p1 + '"' + width + height;
+          } else {
+            return 'src-t="' + p1 + '"' + width + height;
+          }
+        },
+      );
+
+      htmlDataSum += '<div style="display:' + displayStyle + '" id="' + name +
+        '" class="pagediv" title2="' + titleAndSiteName + '">\n';
+      htmlDataSum += htmlData;
+      htmlDataSum += "\n</div>\n";
+
+      allPageMd += "1. [" + title + "](" + name + ")  \n";
+    });
+    const allPageHtmlData = marked(allPageMd);
+    // 組み込み定数の文字を置き換える
+    let tmplJsDataFixed = tmplJsData.replace(
+      "$isDev",
+      !(isDeploy || isStaging) && htmlObj.name == "404" ? "true" : "false",
+    );
+    if (isHelpFile) { // ヘルプファイルの場合JSを除去する
+      tmplJsDataFixed = "";
     }
 
-    const mdDir = path.join(curDir, mdDirName);
-    const mdPaths = base.lsrSync(mdDir);
+    // テンプレートhtmlに、markdownのhtmlを埋め込み
+    let outData: string | Buffer = tmplHtmlData.replace(
+      /%content%/g,
+      htmlDataSum,
+    )
+      .replace(/%title%/g, titleHtmlData)
+      .replace(/%sidebar%/g, sidebarHtmlData)
+      .replace(/%nav_pc%/g, navPcHtmlData)
+      .replace(/%nav_mobile%/g, navMobileHtmlData)
+      .replace(/"%script%"/g, tmplJsDataFixed)
+      .replace(/\/\*%css%\*\//g, tmplCssData)
+      .replace(/%footer%/g, footerHtmlData)
+      .replace(/%allpage%/g, allPageHtmlData);
 
-    // テンプレートhtml, js, css
-    const tmplPath = path.join(curDir, templateDirName, 'template.html');
-    const tmplHtmlData = Buffer.from(fs.readFileSync(tmplPath)).toString();
-    const tmplJsPath = path.join(curDir, templateDirName, 'template.js');
-    const tmplJsData = Buffer.from(fs.readFileSync(tmplJsPath)).toString();
-    const tmplCssPath = path.join(curDir, templateDirName, 'style.css');
-    const tmplCssData = Buffer.from(fs.readFileSync(tmplCssPath)).toString();
+    let htmlPath = path.join(
+      curDir,
+      pubDirName,
+      base.toHtmlPath(htmlObj.mdPath),
+    );
 
-    // 共通部品md
-    const sidebarPath = path.join(mdTemplateDirName, 'tp-sidebar.md');
-    const sidebarHtmlData = base.convMdToHtml(sidebarPath);
-    const navPcPath = path.join(mdTemplateDirName, 'tp-nav-pc.md');
-    const navPcHtmlData = base.convMdToHtml(navPcPath);
-    const navMobilePath = path.join(mdTemplateDirName, 'tp-nav-mobile.md');
-    const navMobileHtmlData = base.convMdToHtml(navMobilePath);
-    const footerPath = path.join(mdTemplateDirName, 'tp-footer.md');
-    const footerHtmlData = base.convMdToHtml(footerPath);
-
-    const htmlAry: { name: string; mdPath: string; data: string; }[] = [];
-    const imgAry: { path: string; width: number; height: number; }[] = [];
-
-    const dlDirPath = path.join(curDir, 'md', 'dl');
-
-    // mdディレクトリ読み込み
-    mdPaths.forEach(mdPath => {
-        if (mdPath.match(/.md$/)) {
-            if (mdPath.match(/tp-.*.md$/)) {
-                return;
-            }
-            // markdown -> html 変換
-            const mdData = fs.readFileSync(mdPath);
-            let htmlData = marked(mdData.toString());
-
-            const mdName = path.parse(mdPath).name;
-            htmlAry.push({
-                name: mdName,
-                mdPath: mdPath,
-                data: htmlData
-            });
-
-            console.log('convert md:', mdPath);
-        } else {
-            // dlフォルダはデプロイ時のみコピー
-            if (!isDeploy && mdPath.indexOf(dlDirPath) != -1) {
-                return;
-            }
-            // 画像のwidth,heightを取得する
-            if (path.parse(mdPath).ext == '.png') {
-                const imgSize = imageSize(mdPath);
-                imgAry.push({
-                    path: mdPath,
-                    width: imgSize.width || 0,
-                    height: imgSize.height || 0
-                });
-            }
-            const relativePath = mdPath.replace(mdDir, '');
-            const inPath = path.join(mdDir, relativePath);
-            const outPath = path.join(curDir, pubDirName, relativePath);
-            if (isFullBuild || base.isUpdateFile(inPath, outPath)) {
-                // ファイルコピー(md -> docs)
-                fs.mkdirSync(path.dirname(outPath), { recursive: true });
-                fs.copyFileSync(inPath, outPath);
-                console.log('copy file :', outPath);
-            }
-        }
-    });
-
-    const time2 = performance.now();
-
-    // htmlを生成
-    htmlAry.forEach(htmlObj => {
-        // デバッグ時は404.htmlのみ作成
-        if (!(isDeploy || isStaging || isHelpFile || htmlObj.name == '404')) {
-            return;
-        }
-        const time = performance.now();
-
-        // 全htmlをまとめたhtmlを生成（content部分のみ）
-        const fileName = htmlObj.name;
-        let titleHtmlData = '';
-        let htmlDataSum = '';
-        let allPageMd = '';
-        htmlAry.forEach(obj => {
-            const name = obj.name;
-            const isSelfPage = name == fileName;
-            // ヘルプファイルの場合は、1つのhtmlに1つのhtmlのみ入れる（1対1）
-            if (isHelpFile && !isSelfPage) {
-                return;
-            }
-            const displayStyle = isSelfPage ? 'block' : 'none';
-
-            let htmlData = obj.data;
-
-            // title取得
-            let title = base.getTitle(htmlData);
-            htmlData = htmlData.replace(/\<title\>(.*)\<\/title\>/g,
-                (match, p1, offset, string) => {
-                    title = p1;
-                    return '';
-                });
-            // title末尾にサイト名を付ける
-            let titleAndSiteName = title;
-            if (name != 'index') {
-                titleAndSiteName += ' - Tonyu1 Wiki';
-            }
-            if (isSelfPage) { // 最初に表示するページ
-                titleHtmlData = titleAndSiteName;
-            }
-
-            // 非表示のページはimgタグsrcをsrc-tにして画像を読まないようにする
-            // 表示時にjs側でsrcに戻す
-            htmlData = htmlData.replace(/src=\"(.*?)\"/g,
-                (match, p1, offset, string) => {
-                    // <img src="xxx.png">を<img src-t="xxx.png">にする
-                    // imgタグにwidth,heightを追加する
-                    const img = imgAry.find(obj => path.join(mdDir, p1).indexOf(obj.path) != -1);
-                    const width = img ? ' width="' + img.width + '" ' : '';
-                    const height = img ? ' height="' + img.height + '" ' : '';
-                    if (isSelfPage) {
-                        return 'src="' + p1 + '"' + width + height;
-                    } else {
-                        return 'src-t="' + p1 + '"' + width + height;
-                    }
-                });
-
-            htmlDataSum += '<div style="display:' + displayStyle + '" id="' + name + '" class="pagediv" title2="' + titleAndSiteName + '">\n';
-            htmlDataSum += htmlData;
-            htmlDataSum += '\n</div>\n';
-
-            allPageMd += '1. [' + title + '](' + name + ')  \n';
-        });
-        const allPageHtmlData = marked(allPageMd);
-        // 組み込み定数の文字を置き換える
-        let tmplJsDataFixed = tmplJsData.replace(
-            '$isDev',
-            !(isDeploy || isStaging) && htmlObj.name == '404' ? 'true' : 'false'
+    if (isDeploy || isHelpFile) {
+      // Github Pagesは.gzファイルを置いても静的gzipとして扱ってくれないので、deploy時は.htmlを置く
+      // ヘルプファイルの場合は.htmlを置く
+      // html, js, css のminify
+      outData = await minify(outData, {
+        continueOnParseError: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        processConditionalComments: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeTagWhitespace: true,
+      });
+    } else {
+      // 開発時は404.htmlのみでSPAするので、404.htmlはルートに作る
+      if (!isStaging && htmlObj.name == "404") {
+        htmlPath = path.join(
+          curDir,
+          rootPubDirName,
+          base.toHtmlPath(htmlObj.mdPath),
         );
-        if (isHelpFile) { // ヘルプファイルの場合JSを除去する
-            tmplJsDataFixed = '';
-        }
+      }
+      // htmlをgzip圧縮
+      const ret = base.compress(base.CompressMode.Gzip, htmlPath, outData);
+      htmlPath = ret.filePath;
+      outData = ret.output;
+    }
 
-        // テンプレートhtmlに、markdownのhtmlを埋め込み
-        let outData: string | Buffer =
-            tmplHtmlData.replace(/%content%/g, htmlDataSum)
-                .replace(/%title%/g, titleHtmlData)
-                .replace(/%sidebar%/g, sidebarHtmlData)
-                .replace(/%nav_pc%/g, navPcHtmlData)
-                .replace(/%nav_mobile%/g, navMobileHtmlData)
-                .replace(/"%script%"/g, tmplJsDataFixed)
-                .replace(/\/\*%css%\*\//g, tmplCssData)
-                .replace(/%footer%/g, footerHtmlData)
-                .replace(/%allpage%/g, allPageHtmlData);
+    fs.mkdirSync(path.dirname(htmlPath), { recursive: true });
+    fs.writeFileSync(htmlPath, outData);
 
-        let htmlPath = path.join(curDir, pubDirName, base.toHtmlPath(htmlObj.mdPath));
+    console.log("write file :", performance.now() - time, htmlPath);
+  });
 
-        if (isDeploy || isHelpFile) {
-            // Github Pagesは.gzファイルを置いても静的gzipとして扱ってくれないので、deploy時は.htmlを置く
-            // ヘルプファイルの場合は.htmlを置く
-            // html, js, css のminify
-            outData = minify(outData, {
-                continueOnParseError: true,
-                collapseWhitespace: true,
-                minifyCSS: true,
-                minifyJS: true,
-                processConditionalComments: true,
-                removeComments: true,
-                removeRedundantAttributes: true,
-                removeTagWhitespace: true
-            });
-        } else {
-            // 開発時は404.htmlのみでSPAするので、404.htmlはルートに作る
-            if (!isStaging && htmlObj.name == '404') {
-                htmlPath = path.join(curDir, rootPubDirName, base.toHtmlPath(htmlObj.mdPath));
-            }
-            // htmlをgzip圧縮
-            const ret = base.compress(base.CompressMode.Gzip, htmlPath, outData);
-            htmlPath = ret.filePath;
-            outData = ret.output;
-        }
-
-        fs.mkdirSync(path.dirname(htmlPath), { recursive: true });
-        fs.writeFileSync(htmlPath, outData);
-
-        console.log('write file :', performance.now() - time, htmlPath);
-    });
-
-    console.log('write time :', performance.now() - time2);
-    console.log('  all time :', performance.now() - time3);
+  console.log("write time :", performance.now() - time2);
+  console.log("  all time :", performance.now() - time3);
 }
-
